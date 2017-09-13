46570078f699b4fc41fe447a3f72c541fcd21f33
Paul Schrum <ptschrum@ncsu.edu>
2017-09-12 21:31:02 -0400

    Added class rm21HorSpiral. It is currently empty.

---
+++ ClassLibrary1/Horizontal/rm21HorSpiral.cs
@@ -0,0 +1,15 @@
+﻿using ptsCogo.Angle;
+using ptsCogo.coordinates;
+using ptsCogo.coordinates.CurvilinearCoordinates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ptsCogo.Horizontal
+{
+    public class rm21HorSpiralc : HorizontalAlignmentBase
+    {
+
+    }
+}
