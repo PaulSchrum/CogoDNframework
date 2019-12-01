ed5a52d35873c4ddc5bd7440a584878aebad53b5
PaulSchrum <paul.schrum@gmail.com>
2019-11-30 21:57:45 -0500

    Added Console app to show basics of Tin model usage.

---
+++ TinConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ptsDigitalTerrainModel;
+
+namespace TinConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var thing in args)
+                Console.WriteLine(thing);
+            var tinModel = ptsDTM.CreateFromLAS(args[0]);
+            var pointCount = tinModel.allPoints.Count;
+            var triangleCount = tinModel.TriangleCount;
+            Console.Write($"Successfully loaded tin Model: {pointCount} points and ");
+            Console.WriteLine($"{triangleCount} triangles.");
+
+            if (args.Length == 1) return;
+
+            
+            if (args[1].ToLower() == "get_elevation")
+            { // inputfile get_elevation 2133966 775569
+                var x = Double.Parse(args[2]);
+                var y = Double.Parse(args[3]);
+                var z = tinModel.getElevation(x, y);
+                Console.WriteLine($"Elevation: {z}");
+            }
+            else if(args[1].ToLower() == "to_obj")
+            { // inputfile to_obj outputfile
+                var outfileName = args[2];
+                tinModel.WriteToWaveFront(outfileName);
+                Console.WriteLine($"Wavefront obj file written: {outfileName}");
+            }
+        }
+    }
+}
